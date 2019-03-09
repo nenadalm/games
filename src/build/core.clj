4559b63ac47f1cb90f6b462e5dd1c73b47b19046
Miloslav Nenadal <nenadalm@gmail.com>
2019-03-09 10:33:25 +0100

    Automatic cache busting and build artifact cleaning for `min` build

---
+++ src/build/core.clj
@@ -0,0 +1,59 @@
+(ns build.core
+  (:require
+   [clojure.edn :as edn]))
+
+(def manifest-path "resources/public/js/manifest.edn")
+(def template-path "resources/public/index.html")
+(def output-path "resources/public/index.html")
+(def artifact-dir "resources/public/js/compiled")
+
+(defn- file-name [path]
+  (.getName (clojure.java.io/file path)))
+
+(defn- manifest->replacements [manifest]
+  (reduce-kv (fn [m k v]
+               (assoc m (file-name k) (file-name v)))
+             {}
+             manifest))
+
+(defn- update-fingerprints [template-content manifest]
+  (let [replacements (manifest->replacements manifest)]
+    (loop [template-content template-content
+           match-replacements (seq replacements)]
+      (if-let [match-replacement (first match-replacements)]
+        (recur (clojure.string/replace template-content
+                                       (key match-replacement)
+                                       (val match-replacement))
+               (next match-replacements))
+        template-content))))
+
+(defn read-manifest []
+  (-> manifest-path
+      slurp
+      edn/read-string))
+
+(defn delete-file-recursively [f]
+  (let [files (some-> f
+                      clojure.java.io/file
+                      (as-> file (if (.exists file) file nil))
+                      file-seq
+                      reverse)]
+    (doseq [file files]
+      (clojure.java.io/delete-file file))))
+
+(defn update-template-fingerprints
+  "Takes care of cache busting by replacing js files in template with alternatives using fingerprints.
+  (replaces e.g. app.js with app-111ce5a.js)"
+  [manifest]
+  (->> (update-fingerprints
+        (slurp template-path)
+        manifest)
+       (spit output-path)))
+
+(defn post-build-hook [& args]
+  (let [manifest (read-manifest)]
+    (update-template-fingerprints manifest)
+    (delete-file-recursively artifact-dir)))
+
+(defn pre-build-hook [& args]
+  (delete-file-recursively "resources/public/js"))
