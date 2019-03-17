b849c3a59467cdb3e387c021278e5abdcc7777e7
Miloslav Nenadal <nenadalm@gmail.com>
2019-03-17 11:10:47 +0100

    Get rid of core.async dependency

--- src/app/game_engine.cljs
+++ src/app/game_engine.cljs
@@ -1,12 +1,11 @@
 (ns app.game-engine
   (:require
-   [cljs.core.async :as a]
    [re-frame.core :as re-frame]
    [app.game :as game]))
 
-(def ^:private game-channel (a/chan))
+(defonce ^:private game-state (volatile! {}))
 
-(defn replace-canvas [state canvas]
+(defn- replace-canvas [state canvas]
   (if canvas
     (let [context (.getContext canvas "2d")]
       (-> state
@@ -21,7 +20,7 @@
          :running true
          :prev-time (.now js/window.performance)))
 
-(defn- game-iter!
+(defn- game-iter
   "Runs single iteration of the game loop (update, render). Returns state."
   [state]
   (let [current-time (.now js/window.performance)
@@ -37,40 +36,42 @@
         context (:context updated-state)]
     (when context
       (game/render game (:settings updated-state) context))
-    (.requestAnimationFrame js/window #(a/put! game-channel [:loop]))
     updated-state))
 
-(defn- start-game!
+(defn- start-game
   "Initializes game. Returns initial game state."
   [{:keys [settings game canvas]}]
-  (let [initial-state (-> {:inputs []
-                           :running true
-                           :settings settings
-                           :game (game/init game)}
-                          (replace-canvas canvas)
-                          unpause)]
-    (a/put! game-channel [:loop])
-    initial-state))
+  (-> {:inputs []
+       :running true
+       :settings settings
+       :game (game/init game)}
+      (replace-canvas canvas)
+      unpause))
+
+(defn- process-event! [state event]
+  (case (first event)
+    :input (if (:running state)
+             (update state :inputs conj (second event))
+             state)
+    :loop (do
+            (.requestAnimationFrame js/window #(vswap! game-state process-event! [:loop]))
+            (game-iter state))
+    :start (do
+             (.requestAnimationFrame js/window #(vswap! game-state process-event! [:loop]))
+             (start-game (second event)))
+    :stop (assoc state :running false)
+    :pause (assoc state :running false)
+    :resume (unpause state)
+    :replace-canvas (replace-canvas state (second event))
+    state))
 
 (defn init []
-  (a/go-loop [state {:inputs []
-                     :running false
-                     :settings {}
-                     :prev-time nil}]
-    (let [event (a/<! game-channel)]
-      (case (first event)
-        :input (recur (if (:running state)
-                        (update state :inputs conj (second event))
-                        state))
-        :loop (recur (game-iter! state))
-        :start (recur (start-game! (second event)))
-        :stop (recur (assoc state :running false))
-        :pause (recur (assoc state :running false))
-        :resume (recur (unpause state))
-        :replace-canvas (recur (replace-canvas state (second event)))
-        (recur state)))))
+  (vreset! game-state {:inputs []
+                       :running false
+                       :settings {}
+                       :prev-time nil}))
 
 (re-frame/reg-fx
  :game-event
  (fn [event]
-   (a/put! game-channel event)))
+   (vswap! game-state process-event! event)))
