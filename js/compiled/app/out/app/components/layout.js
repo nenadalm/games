e999bb2d69cfc0aacefa134ee042140f7cad8a1f
nenadalm <nenadalm@gmail.com>
2023-02-02 17:19:15 +0000

    Deploying to gh-pages from @ nenadalm/games@2213c3c102269c125f6cbe87b03bb4406023413d 🚀

--- js/compiled/app/out/app/components/layout.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.10.597 {:static-fns true, :optimize-constants true}
-goog.provide('app.components.layout');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-app.components.layout.layout = (function app$components$layout$layout(content){
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$_LT__GT_,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$container,content,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$footer,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$content$has_DASH_text_DASH_centered,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$a,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$href,"https://github.com/nenadalm/games"], null),"GitHub repository"], null)], null)], null)], null)], null);
-});
--- js/compiled/app/out/app/config.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.10.597 {:static-fns true, :optimize-constants true}
-goog.provide('app.config');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-app.config.debug_QMARK_ = goog.DEBUG;
-
-/** @define {string} */
-goog.define("app.config.sentry_dsn","");
